bf25499e94fdc79ab9c2a804888a413d4b8f3f06
Erwin <erwinvides@hotmail.com>
2017-12-13 21:30:27 -0600

    delete added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
-mongoose.connect('mongodb://evides:qaz123wsx@ds015584.mlab.com:15584/heroku_zz0b7j74');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://evides:qaz123wsx@ds015584.mlab.com:15584/heroku_zz0b7j74');
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -61,6 +61,26 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+    if (!ObjectID.isValid(id)) {
+        console.log('ObjectId is not valid');
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
+        return res.status(404).send();
+
+    });
+
+});
+
 
 app.listen(port, () => {
     console.log(`Server is working in port  ${port}`);
