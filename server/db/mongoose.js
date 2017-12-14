aaa960257c18baefd23318ebcd28248f13a6890b
Erwin <erwinvides@hotmail.com>
2017-12-13 22:33:47 -0600

    updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://evides:qaz123wsx@ds015584.mlab.com:15584/heroku_zz0b7j74');
-
+// mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://evides:qaz123wsx@ds015584.mlab.com:15584/heroku_zz0b7j74');
+mongoose.connect('mongodb://evides:qaz123wsx@ds015584.mlab.com:15584/heroku_zz0b7j74');
 module.exports = {
     mongoose
 };
